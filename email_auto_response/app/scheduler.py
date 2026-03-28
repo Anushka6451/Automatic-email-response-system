1b31c7994cf96c213ec45ef2fa13e83567c10da1
Anushka6451 <146761726+Anushka6451@users.noreply.github.com>
2026-03-28 23:15:20 +0530

    Delete email_auto_response directory

--- email_auto_response/app/scheduler.py
+++
@@ -1,6 +0,0 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-from app.main import process_emails  # function that reads & replies
-
-scheduler = BackgroundScheduler()
-scheduler.add_job(process_emails, 'interval', seconds=30, max_instances=1)
-scheduler.start()
--- email_auto_response/config.py
+++
@@ -1,5 +0,0 @@
-EMAIL = "anushka.s4202@gmail.com"
-PASSWORD = "pmcgzkpykuroxumf"
-IMAP_SERVER = "imap.gmail.com"
-SMTP_SERVER = "smtp.gmail.com"
-SMTP_PORT = 465
